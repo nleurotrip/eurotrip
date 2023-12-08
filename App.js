d81f0c7ef178f3cc951098a4237f5f2881ad5e20
Noah lustig <noahlustig@Noahs-MacBook-Air.local>
2023-12-08 09:47:54 -0700

    add itinerary tab

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import TempPage from './pages/Temp';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import TempPage from './pages/Temp';
+import ItineraryPage from './pages/ItineraryPage';
+
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-       <Tab.Screen name="Temp" component={TempPage} />
+       <Tab.Screen name="Itinerary" component={ItineraryPage} />
        <Tab.Screen name="Settings" component={TempPage} />
       </Tab.Navigator>
     </NavigationContainer>
   )
-}
+};
 
 export default App;
---
+++ pages/ItineraryPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const ItineraryPage = () => {
+    return (
+        <SafeAreaView>
+            <Text>Itinerary coming soon!</Text>
+        </SafeAreaView>
+    );
+};
+
+export default ItineraryPage;
