0152182c6c319b4faa3722c5e0f4727984e74e28
Noah lustig <noahlustig@Noahs-MacBook-Air.local>
2023-12-08 14:14:39 -0700

    add icons and flags to calendar

--- components/Calendar/Calendar.js
+++ components/Calendar/Calendar.js
@@ -8,25 +8,13 @@ export default function Calendar() {
     <View style={styles.container}>
       <View style={styles.calendarContainer}>
         <View style={styles.rowContainer}>
-          { weekOne.map( d => <CalendarSquare key={d.date} day={d.day} date={d.date} active={d.active} location={d.location} /> ) }
+          { weekOne.map( d => <CalendarSquare key={d.date} day={d.day} date={d.date} active={d.active} location={d.location} icon={d.icon} /> ) }
         </View>
         <View style={styles.rowContainer}>
-          <CalendarSquare />
-          <CalendarSquare />
-          <CalendarSquare />
-          <CalendarSquare />
-          <CalendarSquare />
-          <CalendarSquare />
-          <CalendarSquare />
+          { weekTwo.map( d => <CalendarSquare key={d.date} day={d.day} date={d.date} active={d.active} location={d.location} icon={d.icon} /> ) }
         </View>
         <View style={styles.rowContainer}>
-          <CalendarSquare />
-          <CalendarSquare />
-          <CalendarSquare />
-          <CalendarSquare />
-          <CalendarSquare />
-          <CalendarSquare />
-          <CalendarSquare />
+          { weekThree.map( d => <CalendarSquare key={d.date} day={d.day} date={d.date} active={d.active} location={d.location} icon={d.icon} /> ) }
         </View>
       </View>
     </View>
@@ -39,22 +27,23 @@ styles = StyleSheet.create({
     container: {
         width: '100%',
         minHeight: 50,
-        padding: 10,
         marginVertical: 10,
-        borderWidth: 1,
-        borderColor: 'black'
     },
     calendarContainer: {
         width: '100%',
         minHeight: 150,
         borderWidth: 1,
-        borderColor: 'red'
+        borderColor: 'black',
+        backgroundColor: 'white',
+        borderRadius: 5,
+        padding: 10,
+        margin: 5
     },
     rowContainer: {
       flexDirection: 'row',
       width: '100%',
       justifyContent: "space-evenly",
-      borderWidth: 1,
-      borderColor: 'blue'
+      // borderWidth: 1,
+      // borderColor: 'blue'
     }
 });
--- components/Calendar/CalendarSquare.js
+++ components/Calendar/CalendarSquare.js
@@ -1,21 +1,32 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
+import germanFlag from '../../images/germany.png';
+import englishFlag from '../../images/englishFlag.png';
+import dutchFlag from '../../images/netherlandsFlag.png';
+import americanFlag from '../../images/americanFlag.png';
+import { Icon } from '@rneui/themed';
 
-export default function CalendarSquare({day, date, active, location}) {
+const flags = {'LON': englishFlag, 'BER': germanFlag, 'AMS': dutchFlag, 'PHX': americanFlag};
+const icons = {travel: {name: 'airplanemode-active', type: 'material'}, hotel: {name: 'hotel', type: 'material'}};
+
+export default function CalendarSquare({day, date, active, location, icon}) {
   return (
-    <View style={{...styles.container, backgroundColor: active ? 'white' : 'lightgrey'}}>
-      <Text>{date}</Text>
-      <Text>{location}</Text>
+    <View style={{...styles.container, backgroundColor: active ? '#F8F8FF' : 'lightgrey'}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Text>{date}</Text>
+      </View>
+        <Image source={flags[location]} style={{marginLeft:'auto', marginRight:'auto', height: 20, maxWidth: 30, marginTop: 2}} />
     </View>
   )
 }
 
 styles = StyleSheet.create({
     container: {
-        width: 50,
+        width: 48,
         minHeight: 50,
         padding: 0,
         margin: 0,
+        marginVertical: 1,
         borderWidth: 1,
         borderColor: 'black'
     }
