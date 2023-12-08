26a5f809c0f9c2009f4e3346e33d65bbc999d857
Noah lustig <noahlustig@Noahs-MacBook-Air.local>
2023-12-08 08:57:06 -0700

    add navigation container and temp page

---
+++ pages/Temp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const TempPage = () => {
+    return (
+        <SafeAreaView>
+            <Text>Temp page</Text>
+        </SafeAreaView>
+    );
+};
+
+export default TempPage;
