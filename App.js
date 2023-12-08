6bec7f9d7e16df89c777011557888f59828699d0
Noah lustig <noahlustig@Noahs-MacBook-Air.local>
2023-12-08 08:59:26 -0700

    add tab navigator

--- App.js
+++ App.js
@@ -2,11 +2,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import TempPage from './pages/Temp';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <TempPage />
+      <Tab.Navigator>
+       <Tab.Screen name="Temp" component={TempPage} />
+       <Tab.Screen name="Settings" component={TempPage} />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
