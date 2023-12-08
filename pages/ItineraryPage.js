c78f8e3fd8e8b7feda7fcaa2a126bcee31389f4a
Noah lustig <noahlustig@Noahs-MacBook-Air.local>
2023-12-08 09:57:33 -0700

    add example avatar component to verify rne is working

--- pages/ItineraryPage.js
+++ pages/ItineraryPage.js
@@ -1,3 +1,4 @@
+import { Avatar } from '@rneui/themed';
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 
@@ -5,6 +6,12 @@ const ItineraryPage = () => {
     return (
         <SafeAreaView>
             <Text>Itinerary coming soon!</Text>
+            <Avatar 
+                size={40}
+                rounded
+                title="NL"
+                containerStyle={{backgroundColor: 'blue'}}
+            />
         </SafeAreaView>
     );
 };
