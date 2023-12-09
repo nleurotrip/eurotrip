6d4349e79dce4f6ca2109abea823a89b08bf87c1
Noah lustig <noahlustig@Noahs-MacBook-Air.local>
2023-12-09 11:04:35 -0700

    save existing messages from thread in JSON file to avoid hitting API endlessly.

--- server/openAIAPI.js
+++ server/openAIAPI.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const fs = require('fs');
 const OpenAI = require("openai");
 
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
@@ -11,6 +12,14 @@ async function main() {
     // console.log(threadMessages);
     const messageContent = threadMessages.data.map(d => d.content).reduce((result, item) => result.concat(item) ).map(message => message.text.value);
     console.log( messageContent );
+    const jsonContent = JSON.stringify(messageContent);
+    fs.writeFile("./messages.json", jsonContent, 'utf8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+    }); 
 
 //   messages = client.beta.threads.messages.list(
 //     thread_id=thread.id
