b7f6cb3a6fb32350745df93d45d69e906bfd6257
Noah lustig <noahlustig@Noahs-MacBook-Air.local>
2023-12-08 17:56:00 -0700

    add custom icon on navigation tabs

--- Navigation.js
+++ Navigation.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from '@rneui/themed';
 
 import TempPage from './pages/Temp';
 import ItineraryPage from './pages/ItineraryPage';
@@ -12,8 +13,23 @@ const Navigation = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-       <Tab.Screen name="Itinerary" component={ItineraryPage} />
-       <Tab.Screen name="Settings" component={TempPage} />
+       <Tab.Screen 
+        name="Itinerary" 
+        component={ItineraryPage} 
+        options={{
+          tabBarIcon: ({size,focused,color}) => {
+              return (
+                <Icon
+                  name="list-alt"
+                  style={{ width: size, height: size }}
+                  type="material"
+                  color="lightblue"
+                />
+              );
+            }
+        }}
+      />
+       <Tab.Screen name="Points of Interest" component={TempPage} />
       </Tab.Navigator>
     </NavigationContainer>
   )
