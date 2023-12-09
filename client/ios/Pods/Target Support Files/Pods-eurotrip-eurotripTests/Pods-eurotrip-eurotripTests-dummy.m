af3df86f2b3f459309234117638393c71ff65939
Noah lustig <noahlustig@Noahs-MacBook-Air.local>
2023-12-09 09:47:28 -0700

    install openai package

---
+++ client/ios/Pods/Target Support Files/Pods-eurotrip-eurotripTests/Pods-eurotrip-eurotripTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_eurotrip_eurotripTests : NSObject
+@end
+@implementation PodsDummy_Pods_eurotrip_eurotripTests
+@end
---
+++ client/ios/Pods/Target Support Files/Pods-eurotrip/Pods-eurotrip-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_eurotrip : NSObject
+@end
+@implementation PodsDummy_Pods_eurotrip
+@end
