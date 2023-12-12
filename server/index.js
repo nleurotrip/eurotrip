33cfa493ed6936b1e5416ba9795c8e3cef4989e4
Noah lustig <noahlustig@Noahs-MacBook-Air.local>
2023-12-12 06:44:57 -0700

    add list messages route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { listMessages } = require('./openAIAPI');
 
-app.get('/', (req, res) => {
+app.get('/messages/list', async (req, res) => {
+  //get all messages in the thread.
+  let resp;
+  try {
+    resp = await listMessages();
+    console.log('RESP', resp);
+  } catch (e) {
+    throw new Error('Error listing messages in this thread.');
+  }
+  
+  res.send(resp);
+})
+
+
+app.post('/messages/send', (req, res) => {
+  //send
   res.send('Hello World!')
 })
 
--- server/openAIAPI.js
+++ server/openAIAPI.js
@@ -4,27 +4,25 @@ const OpenAI = require("openai");
 
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
-async function main() {
-    // const myAssistant = await openai.beta.assistants.retrieve(
-    //     process.env.ASSISTANT_ID
-    // );
-    const threadMessages = await openai.beta.threads.messages.list(process.env.THREAD_ID);
-    // console.log(threadMessages);
-    const messageContent = threadMessages.data.map(d => d.content).reduce((result, item) => result.concat(item) ).map(message => message.text.value);
-    console.log( messageContent );
-    const jsonContent = JSON.stringify(messageContent);
-    fs.writeFile("./messages.json", jsonContent, 'utf8', function (err) {
-        if (err) {
-            return console.log(err);
-        }
+async function listMessages() {
+
+    const threadMessages = await openai.beta.threads.messages.list(process.env.THREAD_ID, {limit: 10, order: 'asc'});
+    // console.log(threadMessages.data[0]);
+    const messageData = threadMessages.data.map(m => ({role: m.role, content: m.content[0].text.value, createdAt: m.created_at}) );
+    console.log(messageData);
+    return messageData;
+   
+
+    // const jsonContent = JSON.stringify(messageContent);
+    // fs.writeFile("./messages.json", jsonContent, 'utf8', function (err) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
     
-        console.log("The file was saved!");
-    }); 
+    //     console.log("The file was saved!");
+    // }); 
+};
 
-//   messages = client.beta.threads.messages.list(
-//     thread_id=thread.id
-//   )
-//   console.log(completion.choices[0]);
-}
+// listMessages();
 
-main();
+module.exports.listMessages = listMessages;
